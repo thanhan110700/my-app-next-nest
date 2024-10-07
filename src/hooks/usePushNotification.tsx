19f3ed01e3cd4a233e47ea00be2eac6909f893ea
AnLVT-Kozocom <anlvt@kozo-japan.com>
2024-10-07 15:40:05 +0700

    test

--- src/hooks/usePushNotification.tsx
+++ src/hooks/usePushNotification.tsx
@@ -7,39 +7,56 @@ const PushNotifications = () => {
   const [value, setValue] = useState<any>();
   const handleClick = () => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js").then((registration) => {
-        registration?.pushManager?.getSubscription().then((sub) => {
-          if (sub === null) {
-            registration.pushManager
-              .subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
-              })
-              .then((subscription) => {
-                const subscriptionObject = JSON.stringify(subscription);
-                try {
-                  setValue(
-                    JSON.stringify({
-                      ...JSON.parse(subscriptionObject),
-                    })
-                  );
-                  fetch(`${API_URL}/save-subscription`, {
-                    method: "POST",
-                    body: JSON.stringify({
-                      ...JSON.parse(subscriptionObject),
-                      key: APP_KEY,
-                    }),
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          registration?.pushManager
+            ?.getSubscription()
+            .then((sub) => {
+              if (sub === null) {
+                registration.pushManager
+                  .subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey:
+                      process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
+                  })
+                  .then((subscription) => {
+                    const subscriptionObject = JSON.stringify(subscription);
+                    try {
+                      setValue(
+                        JSON.stringify({
+                          ...JSON.parse(subscriptionObject),
+                        })
+                      );
+                      fetch(`${API_URL}/save-subscription`, {
+                        method: "POST",
+                        body: JSON.stringify({
+                          ...JSON.parse(subscriptionObject),
+                          key: APP_KEY,
+                        }),
+                        headers: {
+                          "Content-Type": "application/json",
+                        },
+                      });
+                    } catch (error) {
+                      console.log(error);
+                    }
+                  })
+                  .catch((error) => {
+                    console.log(error.message);
+                    setValue(error.message);
                   });
-                } catch (error) {
-                  console.log(error);
-                }
-              });
-          }
+              }
+            })
+            .catch((error) => {
+              console.log(error.message);
+              setValue(error.message);
+            });
+        })
+        .catch((error) => {
+          console.log(error.message);
+          setValue(error.message);
         });
-      });
     }
   };
 
